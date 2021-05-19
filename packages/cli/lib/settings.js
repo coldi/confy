63ba6041752c6c2e9cdd6756cdcf5753a875b227
Felix Laukel <felix.laukel@milchundzucker.de>
2021-05-19 16:00:42 +0200

    Revert "Fix issue with cli using latest unstable version"
    This reverts commit c0fbbc5ea4cd2391f2a7d7b761e952927bb8104c.

--- packages/cli/lib/settings.js
+++ packages/cli/lib/settings.js
@@ -3,13 +3,10 @@ const appPath = process.cwd();
 const presetNameRegEx = /^[a-z0-9-]+$/i;
 const prefix = '@confy/';
 const presetNamePrefix = `${prefix}preset-`;
-// Workaround to avoid installing latest 1.0.0-beta* releases
-const presetVersionSuffix = '@^0.x.x';
 
 module.exports = {
     appPath,
     prefix,
     presetNameRegEx,
     presetNamePrefix,
-    presetVersionSuffix,
 };
